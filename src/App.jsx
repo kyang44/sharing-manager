33d7a0b01bf229d3c645c1c0c74e75c6cb98880a
Kody Yang <115596102+kyang44@users.noreply.github.com>
2023-12-30 20:56:13 -0600

    Added upload to dropbox

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,38 @@ import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import parse from "html-react-parser";
 import "./App.css";
+import { Dropbox } from "dropbox";
+
+const dbx = new Dropbox({
+  accessToken:
+    "sl.BszL3MEFQ99QhA0gvFYuhZC7soPWSIN9MmbmwbcYYbu_1TyosghoQRDNyUU-RsdDbPY9-AN1xrbWywTm60bxr8RHF_xltffAAUNkcv5qetfIMniWDu8ZOtasA8RJqkGGWalVuD6sFCau",
+});
 
 function App() {
   const [text, setText] = useState("");
+  const [fileName, setFileName] = useState("");
+
+  async function saveFileContent() {
+    try {
+      await dbx.filesUpload({
+        path: "/" + fileName,
+        contents: text,
+      });
+      console.log("File saved to Dropbox");
+    } catch (error) {
+      console.error("Error saving file:", error);
+      console.error("Dropbox API error:", error.response);
+    }
+  }
+
+  async function handleSave() {
+    const newFileName = window.prompt("Enter file name");
+    if (newFileName) {
+      setFileName(newFileName);
+      await saveFileContent();
+    }
+  }
+
   return (
     <>
       <div className="App">
@@ -18,6 +47,7 @@ function App() {
               setText(data);
             }}
           />
+          <button onClick={() => handleSave()}>Save</button>
         </div>
         <div>
           <h2>Content</h2>
