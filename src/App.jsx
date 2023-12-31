e9166f35d4c1615a3edd6dbacd88cc8bcba5b8fc
Kody Yang <115596102+kyang44@users.noreply.github.com>
2023-12-30 18:45:03 -0600

    Added editor component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import parse from "html-react-parser";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [text, setText] = useState("");
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <div className="editor">
+          <CKEditor
+            editor={ClassicEditor}
+            data={text}
+            onChange={(e, editor) => {
+              const data = editor.getData();
+              setText(data);
+            }}
+          />
+        </div>
+        <div>
+          <h2>Content</h2>
+          <p>{parse(text)}</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
